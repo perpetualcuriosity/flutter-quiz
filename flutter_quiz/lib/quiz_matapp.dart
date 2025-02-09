4317c6794aa2ceb8a4bb1f254f24dfe3a6be8eae
Jash Upadhyay <jashupadhyay.work@gmail.com>
2025-02-09 10:36:05 +0530

    scaffold linked to materialapp

--- flutter_quiz/lib/quiz_matapp.dart
+++ flutter_quiz/lib/quiz_matapp.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
-class MatApp extends StatelessWidget
-{
-    const MatApp({super.key});
-    @override
-    Widget build(BuildContext context)
-    {
-        return MaterialApp(
-          debugShowCheckedModeBanner:true
-        );
-    }
+import 'package:flutter_quiz/quiz_scaffold.dart';
+
+class MatApp extends StatelessWidget {
+  const MatApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: true,
+      home: Scaff(),
+    );
+  }
 }
