fbd4a0681cf747001e43a819ae49d5938cfb89a2
Jash Upadhyay <jashupadhyay.work@gmail.com>
2025-02-09 10:27:47 +0530

    main.dart connected to materialapp, materialapp written

---
+++ flutter_quiz/lib/quiz_matapp.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+class MatApp extends StatelessWidget
+{
+    const MatApp({super.key});
+    @override
+    Widget build(BuildContext context)
+    {
+        return MaterialApp(
+          debugShowCheckedModeBanner:true
+        );
+    }
+}
