b7ae67166e4657ddb7e141a4642e663c218e54c4
Jash Upadhyay <jashupadhyay.work@gmail.com>
2025-02-09 10:35:34 +0530

    scaffold basic created

---
+++ flutter_quiz/lib/quiz_scaffold.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+class Scaff extends StatefulWidget
+{
+    const Scaff({super.key});
+    @override
+    State<Scaff> createState() => _Scaff();
+}
+class _Scaff extends State<Scaff>
+{
+    @override
+    Widget build(BuildContext context)
+    {
+      return Scaffold(
+        
+      );
+    }
+}
