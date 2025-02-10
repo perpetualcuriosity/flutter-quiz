64b06ecc27c2c0e588224d04de0f6eae729847e2
Jash Upadhyay <jashupadhyay.work@gmail.com>
2025-02-10 23:41:09 +0530

    quiz widget created and set up done for navigating to quiz from landing page

---
+++ flutter_quiz/lib/quiz.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+class Quiz extends StatefulWidget
+{
+    const Quiz({super.key});
+    @override
+    State<Quiz> createState() => _Quiz();
+}
+class _Quiz extends State<Quiz>
+{
+    @override
+    Widget build(BuildContext context) 
+    {
+        return Column();
+    }
+}
--- flutter_quiz/lib/quiz_scaffold.dart
+++ flutter_quiz/lib/quiz_scaffold.dart
@@ -9,41 +9,35 @@ class Scaff extends StatefulWidget {
 
 class _Scaff extends State<Scaff> {
   Widget wid = Container(
-        decoration: BoxDecoration(
-            gradient: LinearGradient(
-                colors: col,
-                begin: Alignment.topLeft,
-                end: Alignment.bottomRight)),
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            mainAxisSize: MainAxisSize.max,
-            children: [
-              Image.asset('lib/assets/quiz-logo.png', height: 300, width: 300),
-              SizedBox(height: 30),
-              const Text('Learn Flutter The Fun Way!',
-                  style: TextStyle(
-                      color: Colors.white, fontWeight: FontWeight.bold)),
-              SizedBox(height: 30),
-              OutlinedButton.icon(
-                onPressed: () {
-                  
-                },
-                icon: Icon(Icons.arrow_right_alt, color: Colors.white),
-                label: Text(
-                  'Start Quiz',
-                  style: TextStyle(
-                      color: Colors.white, fontWeight: FontWeight.bold),
-                ),
-              )
-            ],
-          ),
-        ),
-      );
+    decoration: BoxDecoration(
+        gradient: LinearGradient(
+            colors: col, begin: Alignment.topLeft, end: Alignment.bottomRight)),
+    child: Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        mainAxisSize: MainAxisSize.max,
+        children: [
+          Image.asset('lib/assets/quiz-logo.png', height: 300, width: 300),
+          SizedBox(height: 30),
+          const Text('Learn Flutter The Fun Way!',
+              style:
+                  TextStyle(color: Colors.white, fontWeight: FontWeight.bold)),
+          SizedBox(height: 30),
+          OutlinedButton.icon(
+            onPressed: () {},
+            icon: Icon(Icons.arrow_right_alt, color: Colors.white),
+            label: Text(
+              'Start Quiz',
+              style:
+                  TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+            ),
+          )
+        ],
+      ),
+    ),
+  );
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: wid 
-    );
+    return Scaffold(body: wid);
   }
 }
