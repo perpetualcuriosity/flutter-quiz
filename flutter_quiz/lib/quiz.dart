1721d0d06c1bcce394f7e4f5255d4ff27c9ed7f9
Jash Upadhyay <jashupadhyay.work@gmail.com>
2025-02-11 00:11:21 +0530

    experimenting more with navigation

--- flutter_quiz/lib/quiz.dart
+++ flutter_quiz/lib/quiz.dart
@@ -1,15 +1,30 @@
 import 'package:flutter/material.dart';
-class Quiz extends StatefulWidget
-{
-    const Quiz({super.key});
-    @override
-    State<Quiz> createState() => _Quiz();
+import 'package:flutter_quiz/global_theme.dart';
+
+class Quiz extends StatefulWidget {
+  const Quiz({super.key});
+  @override
+  State<Quiz> createState() => _Quiz();
 }
-class _Quiz extends State<Quiz>
-{
-    @override
-    Widget build(BuildContext context) 
-    {
-        return Text('Question Screen');
-    }
+
+class _Quiz extends State<Quiz> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(gradient: LinearGradient(colors: col)),
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            Text(
+              'Quiz Page',
+              style:
+                  TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 }
--- flutter_quiz/lib/quiz_scaffold.dart
+++ flutter_quiz/lib/quiz_scaffold.dart
@@ -10,42 +10,48 @@ class Scaff extends StatefulWidget {
 
 class _Scaff extends State<Scaff> {
   Widget wid = Container();
-  @override
-  void initState() {
+  void initstate() {
     super.initState();
+    void screenchanger() {
+      setState(() {
+        wid = Quiz();
+      });
+    }
+
     wid = Container(
-    decoration: BoxDecoration(
-        gradient: LinearGradient(
-            colors: col, begin: Alignment.topLeft, end: Alignment.bottomRight)),
-    child: Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        mainAxisSize: MainAxisSize.max,
-        children: [
-          Image.asset('lib/assets/quiz-logo.png', height: 300, width: 300),
-          SizedBox(height: 30),
-          const Text('Learn Flutter The Fun Way!',
-              style:
-                  TextStyle(color: Colors.white, fontWeight: FontWeight.bold)),
-          SizedBox(height: 30),
-          OutlinedButton.icon(
-            onPressed: () {
-              setState(() {
-                wid = Quiz();
-              });
-            },
-            icon: Icon(Icons.arrow_right_alt, color: Colors.white),
-            label: Text(
-              'Start Quiz',
-              style:
-                  TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
-            ),
-          )
-        ],
+      decoration: BoxDecoration(
+          gradient: LinearGradient(
+              colors: col,
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight)),
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            Image.asset('lib/assets/quiz-logo.png', height: 300, width: 300),
+            SizedBox(height: 30),
+            const Text('Learn Flutter The Fun Way!',
+                style: TextStyle(
+                    color: Colors.white, fontWeight: FontWeight.bold)),
+            SizedBox(height: 30),
+            OutlinedButton.icon(
+              onPressed: () {
+                screenchanger();
+              },
+              icon: Icon(Icons.arrow_right_alt, color: Colors.white),
+              label: Text(
+                'Start Quiz',
+                style:
+                    TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+              ),
+            )
+          ],
+        ),
       ),
-    ),
-  ); // Set the initial UI
+    );
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(body: wid);
